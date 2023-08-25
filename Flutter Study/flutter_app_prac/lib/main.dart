b589c19cc4229067555cc7bc0bb12d23c40618d5
Ji soo Jeong <jpy1030@naver.com>
2023-08-25 22:52:45 +0900

    230825 flutter study

--- Flutter Study/flutter_app_prac/lib/main.dart
+++ Flutter Study/flutter_app_prac/lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter_application_prac/screen/home_screen_S5.dart';
 import 'package:flutter_application_prac/screen/home_screen_S6.dart';
 
 void main() {
+  // Flutter가 앱을 실행할 준비가 될 때까지 기다린다. 
+  WidgetsFlutterBinding.ensureInitialized();
+  
   // runApp 플러터의 진입 포인트
   runApp(
     // Widget - 클래스의 일종
