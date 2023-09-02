f10dfdda79c8cd73aaf55a7c67a2d7abeadaaf7b
StopSoo <jpy1030@naver.com>
2023-09-02 20:07:12 +0900

    230902 flutter study

--- Flutter Study/flutter_app_prac/lib/screen/homeScreen/home_screen_S14.dart
+++ Flutter Study/flutter_app_prac/lib/screen/homeScreen/home_screen_S14.dart
@@ -1,5 +1,7 @@
 // Section 14 : Navigation
+// MainLayout 적용
 import 'package:flutter/material.dart';
+import 'package:flutter_application_prac/layout/main_layout.dart';
 import 'package:flutter_application_prac/screen/route_one_screen.dart';
 
 class HomeScreenS14 extends StatelessWidget {
@@ -7,29 +9,20 @@ class HomeScreenS14 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Screen')
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => RouteOneScreen(),
-                  )
-                );
-              }, 
-              child: Text('Push')
-            )
-          ],
-        ),
-      )
+    return MainLayout(
+      title: 'Home Screen', 
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) => RouteOneScreen(),
+              )
+            );
+          }, 
+          child: Text('Push')
+        )
+      ],
     );
   }
 }
--- Flutter Study/flutter_app_prac/lib/screen/route_one_screen.dart
+++ Flutter Study/flutter_app_prac/lib/screen/route_one_screen.dart
@@ -1,30 +1,23 @@
 // Section 14 : Navigation
+// MainLayout 적용
 import 'package:flutter/material.dart';
+import 'package:flutter_application_prac/layout/main_layout.dart';
 
 class RouteOneScreen extends StatelessWidget {
   const RouteOneScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Route One')
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              }, 
-              child: Text('Pop')
-            )
-          ],
-        ),
-      )
+    return MainLayout(
+      title: 'Route One', 
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          }, 
+          child: Text('Pop')
+        )
+      ],
     );
   }
 }
