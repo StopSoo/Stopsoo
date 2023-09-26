6276b135cebb69cc1d0c5bb4346716cc3106d44b
StopSoo <jpy1030@naver.com>
2023-09-26 20:07:49 +0900

    230926 swift study

--- Swift Study/basicGrammar_5.swift
+++ Swift Study/basicGrammar_5.swift
@@ -293,3 +293,218 @@ jisoo?.home?.guard?.job = nil
 
 guardJob = jisoo?.home?.guard?.job ?? "슈퍼맨"
 print(guardJob) // 슈퍼맨
+
+
+/* 23. 타입 캐스팅 */
+// 스위프트의 타입 캐스팅은 인스턴스의 타입을 확인하는 용도
+// 또는 클래스의 인스턴스를 부모 혹은 자식 클래스의 타입으로 사용할 수 있는지 확인하는 용도로 사용
+// is, as를 사용
+
+/* 잘못된 예시
+해당 예시는 someDouble 변수를 Double형 변수로 선언하며, someInt 변수가 가진 값을 넣어주는 코드
+let someInt: Int = 100
+let someDouble: Double = Double(someInt)
+*/
+
+/* 타입 캐스팅을 위한 클래스 정의 */
+class Person2 {
+    var name: String = ""
+    func breath() {
+        print("숨을 쉽니다.")
+    }
+}
+
+class Student2: Person1 {
+    var school: String = ""
+    func goToSchool() {
+        print("등교를 합니다.")
+    }
+}
+
+class UniversityStudent: Student2 {
+    var major: String = ""
+    func goToMT() {
+        print("MT를 갑니다.")
+    }
+}
+
+var jisoo: Person2 = Person2()
+var sieun: Student2 = Student2()
+var jason: UniversityStudent = UniversityStudent()
+
+/* 타입 확인 -> is 사용 */
+var result: Bool
+
+result = jisoo is Person2           // T
+result = jisoo is Student2          // F
+result = jisoo is UniversityStudent // F
+
+result = sieun is Person2           // T
+result = sieun is Student2          // T
+result = sieun is UniversityStudent // F
+
+result = jason is Person2           // T
+result = jason is Student2          // T
+result = jason is UniversityStudent // T
+
+// 아래와 같이 사용 가능 
+if jisoo is UniversityStudent {
+    print("jisoo는 대학생입니다.")
+} else if jisoo is Student2 {
+    print("jisoo는 학생입니다.")
+} else if jisoo is Person2 {
+    print("jisoo는 사람입니다.")
+} 
+
+switch jason {
+case is Person2:
+    print("jason은 사람입니다.")
+case is Student2:
+    print("jason은 학생입니다.")
+/* ... */
+}
+
+/* 업 캐스팅*/
+// as를 사용하여 부모 클래스의 인스턴스로 사용할 수 있도록 컴파일러에게 타입 정보를 전환해줌
+// Any 혹은 AnyObject로도 타입 정보 변환 가능
+// 암시적으로 처리되므로 생략해도 무방
+
+var mike: Person2 = UniversityStudent() as Person2
+var jenny: Student2 = Student2()
+// var jina: UniversityStudent = Person() as UniversityStudent // 컴파일 오류
+var jina: Any = Person2()   // as Any 생략 가능
+
+/* 다운 캐스팅 */
+/*
+as? 또는 as!를 사용하여 자식 클래스의 인스턴스로 사용할 수 있도록
+컴파일러에게 인스턴스의 타입 정보를 전환해줌
+*/
+
+/* 조건부 다운 캐스팅 */
+// as?
+// 캐스팅에 성공하면 옵셔널을 반환
+var optionalCasted: Student2?
+
+optionalCasted = mike as? UniversityStudent
+optionalCasted = jenny as? UniversityStudent    // nil
+optionalCasted = jina as? UniversityStudent     // nil
+optionalCasted = jina as? Student2  // nil
+
+/* 강제 다운 캐스팅 */
+// as!
+// 반환 타입이 옵셔널이 아니고 일반 타입이라 편하게 활용 가능
+var forcedCasted: Student2 
+
+optionalCasted = mike as! UniversityStudent
+// optionalCasted = jenny as! UniversityStudent    // 런타임 오류
+// optionalCasted = jina as! UniversityStudent // 런타임 오류
+// optionalCasted = jina as! Student2  // 런타임 오류
+
+// 활용 
+// 1. 캐스팅을 하려면 아래와 같이 따로 써줘야 한다. 
+func doSomethingWithSwitch(someone: Person) {
+    switch someone {
+        case is UniversityStudent:
+            (someone as! UniversityStudent).goToMT()
+        case is Student2:
+            (someone as! Student).goToSchool()
+        case is Person2:
+            (someone as! Person2).breath()
+    }   
+}
+
+doSomethingWithSwitch(someone: mike as Person2)
+doSomethingWithSwitch(someone: mike)
+doSomethingWithSwitch(someone: jenny)   // 등교를 합니다.
+
+// 2. 캐스팅과 동시에 가능 (옵셔널 값을 받아옴)
+func doSomething(someone: Person2) {
+    if let universityStudent = someone as? UniversityStudent {
+        universityStudent.goToMT()
+    } else if let student = someone as? Student2 {
+        student.goToSchool()
+    } else if let person = someone as? Person2 {
+        person.breath()
+    }
+}
+
+doSomething(someone: mike as Person2)
+doSomething(someone: mike)
+doSomething(someone: jenny)
+doSomething(someone: jisoo)
+
+
+/* 24. assert와 guard */
+/* Assertion */
+/*
+- assert(_:_:file:line:) 함수를 사용
+- assert 함수는 디버깅 모드에서만 동작, 배포하는 애플리케이션에서는 제외됨
+- 주로 디버깅 중 조건의 검증을 위하여 사용
+*/
+
+var someInt: Int = 0
+// someInt == 0을 검사
+// 맞을 경우는 그냥 지나치지만, 아닐 경우 해당 메시지를 반환하고 중지시킴
+assert(someInt == 0, "someInt != 0")
+
+someInt = 1
+// assert(someInt == 0) // 동작 중지, 검증 실패
+// assert(someInt == 0, "someInt != 0") // 동작 중지, 검증 실패 후 아래와 같은 메시지 출력
+// assertion failed: someInt != 0: file guard_assert.swift, line 26
+
+func functionWithAssert(age: Int?) {
+    assert(age != nil, "age == nil")
+    assert((age! >= 0) && (age! <= 130), "나이값 입력이 잘못되었습니다.")
+    // assert 구문을 다 통과하면 아래 print문이 실행됨
+    print("당신의 나이는 \(age!)세입니다.")
+}
+functionWithAssert(age: 50)
+// functionWithAssert(age: -1) // 동작 중지, 검증 실패
+// functionWithAssert(age: nil)    // 동작 중지, 검증 실패
+
+/* Early Exit */
+/*
+- guard를 사용하여 잘못된 값의 전달 시 특정 실행 구문을 빠르게 종료
+- 디버깅 모드 뿐만 아니라 어떤 조건에서도 동작
+- guard의 else 블럭 내부에는 특정 코드 블럭을 종료하는 지시어(return, break 등)가 꼭 있어야 함
+- 타입 캐스팅, 옵셔널과도 자주 사용됨
+- 그 외 단순 조건 판단 후 빠르게 종료할 때도 용이
+*/
+
+func functionWithGuard(age: Int?) {
+    guard let unwrappedAge = age,   // 옵셔널 바인딩을 먼저 실행 
+        unwrappedAge < 130,
+        unwrappedAge >= 0 else {
+            print("나이값 입력이 잘못되었습니다.")
+            return
+        }
+    // guard는 if-let과 다르게 매개변수를 아래에서도 사용할 수 있게 해준다 (!)
+    print("당신의 나이는 \(unwrappedAge)세입니다.")
+}
+
+var count = 1
+while true {
+    guard count < 3 else {
+        break
+    }
+    print(count)
+    count += 1
+}
+// 1
+// 2
+
+func someFunction(info: [String: Any]) {
+    guard let name = info["name"] as? String else {
+        return
+    }
+    
+    guard let age = info["age"] as? Int, age >= 0 else {
+        return
+    }
+
+    print("\(name): \(age)")
+}
+
+someFunction(info: ["name" : "jenny", "age": "10"])
+someFunction(info: ["name" : "mike"])
+someFunction(info: ["name" : "jisoo", "age": 10])   // jisoo: 10
