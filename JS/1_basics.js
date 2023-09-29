90a4d34e1a6f23d393f0aba65816f1153a7e55a9
StopSoo <jpy1030@naver.com>
2023-09-29 13:23:48 +0900

    230929 js study

--- JS/1_basics.js
+++ JS/1_basics.js
@@ -203,8 +203,173 @@ console.log(season);
 console.log(season[2]);
 // season[0] = '보옴'  // 수정
 console.log(typeof season);
+console.log('----------------');
 
 /**
  * static typing -> 변수를 선언할 때 어떤 타입의 변수를 선언할지를 명시
  * dynamic typing -> 변수의 타입을 명시적으로 선언하지 않고 값에 의해 타입이 지정됨
  */
+
+
+/**
+ * Hoisting
+ * 
+ * 모든 변수 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상
+ * var, let, const 모두 hoisting 발생
+ * but, var은 hoisting 현상을 막아주지 못하지만, let, const는 막아주므로 let, const 위주로 사용할 것
+ */
+console.log('Hello');
+console.log('World');
+
+console.log(name);  // 변수 선언 전에 호출 -> undefined가 출력됨
+var name = '지수';
+console.log(name);
+
+// console.log(yujin); // yujin 변수에 초기화 전에 접근할 수 없다는 오류 메시지가 출력됨
+let yujin = '유진';
+console.log('----------------');
+
+
+/**
+ * Operators
+ */
+
+/**
+ * 증감 연산자 사용 가능
+ */
+let num = 1;
+num++;
+console.log(num);
+console.log('----------------');
+
+/**
+ * 숫자가 아닌 타입에 +, -를 사용한다면 ?
+ */
+let sample = '99';
+console.log(+sample);   // 99
+console.log(typeof +sample);    // number
+
+console.log(sample);    // 99
+console.log(typeof sample); // string - 타입이 변경되진 않음 
+
+sample = true;
+console.log(+sample);   // 1 - true의 숫자 변환값이 1이라는 것을 알 수 있음
+console.log(typeof +sample);    // number
+
+sample = false;
+console.log(+sample);   // 0 - false의 숫자 변환값이 0이라는 것을 알 수 있음
+console.log(typeof +sample);    // number
+
+sample = '지수';
+console.log(+sample);   // NaN (Not a Number) - string은 변환X
+
+sample = '99';
+console.log(-sample);   // -99
+console.log(typeof -sample);    // number
+console.log('----------------');
+
+/**
+ * 할당 연산자 사용 가능
+ */
+var number = 100;
+console.log(number);
+
+number += 10;
+console.log(number);    // 110
+console.log('----------------');
+
+/**
+ * 비교 연산자
+ * 
+ * 1) 값의 비교
+ * 2) 값과 타입의 비교 (!)
+ */
+
+// 1) 값의 비교
+// '같다'는 ==, '같지 않다'는 !=을 사용
+console.log(5 == 5);    // true
+console.log(5 == '5');  // true
+console.log(0 == '');   // true
+console.log(true == 1); // true
+console.log(false == 0);    // true
+console.log(true == '1');   // true
+console.log('----------------');
+
+// 위 과정이 굉장히 이상하므로 주로 2번을 사용한다.
+// 2) 값과 타입의 비교
+// ===을 사용하면 변환되는 값이 같더라도 타입이 다르면 false가 출력됨
+// '같다'는 ===, '같지 않다'는 !==을 사용
+console.log(5 === 5);   // true
+console.log(5 === '5');  // false
+console.log(0 === '');   // false
+console.log(true === 1); // false
+console.log(false === 0);    // false
+console.log(true === '1');   // false
+console.log('----------------');
+
+/**
+ * 삼항 조건 연산자 사용 가능
+ * ternary operator
+ * 조건 ? 조건이 참일 때 : 조건이 거짓일 때
+ */
+
+/**
+ * 단축 평가 (short circuit evaluation)
+ * 
+ * &&를 사용했을 때 좌측이 true면 우측 값 반환 | false면 좌측 값 반환 
+ * ||를 사용했을 때 좌측이 true면 좌측 값 반환 | false면 우측 값 반환
+ */
+
+/**
+ * 지수 연산자
+ */
+console.log(2 ** 2);
+console.log(10 ** 3);
+console.log('----------------');
+
+/**
+ * null 연산자
+ */
+let myName;
+myName = myName ?? '정지수';    // 변수 name이 undefined일 경우에 오른쪽 값을 할당해라
+// name ??= '정지수';   // 이렇게 사용할 수도 있음
+console.log(myName);
+console.log('----------------');
+
+
+/**
+ * If and Switch
+ * 
+ * if - else if - else
+ */
+let myNumber = 5;
+if (myNumber % 2 === 0) {
+    console.log("짝수입니다.")
+} else {
+    console.log("홀수입니다.")
+}
+console.log('----------------');
+
+const englishDay = 'saturday';
+let koreanDay;
+
+switch(englishDay) {
+    case 'monday':
+        koreanDay = '월요일';
+        break;  // break문 반드시 사용 !
+    case 'tuesday':
+        koreanDay = '화요일';
+        break;
+    case 'wednesday':
+        koreanDay = '수요일';
+    case 'thursday':
+        koreanDay = '목요일';
+    case 'friday':
+        koreanDay = '금요일';
+    default:
+        koreanDay = '주말';
+}
+console.log(koreanDay)
+console.log('----------------');
+
+
