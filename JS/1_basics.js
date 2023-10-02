0fc612d6d7d418f98eed7ac7772cd608370a0dc9
StopSoo <jpy1030@naver.com>
2023-10-02 17:09:43 +0900

    231002 js study

--- JS/1_basics.js
+++ JS/1_basics.js
@@ -438,6 +438,7 @@ do {
 console.log(k);
 console.log('-----------');
 
+
 /**
  * Coersion
  * 타입 변환
@@ -492,5 +493,126 @@ console.log(!!null);  // null을 boolean 타입으로 변환했을 때는 false
 
 console.log(!!{});  // Object는 비어 있든 안 비어 있든 무조건 true
 console.log(!![]);  // array도 비어 있든 안 비어 있든 무조건 true
+console.log('-----------');
 
 
+/**
+ * function
+ * 
+ * DRY : Don't Repeat Yourself
+ */
+
+function calculate(_num) {
+    console.log((_num * 10 / 2 % 3).toString());
+}
+calculate(4);    // 함수 실행
+
+/**
+ * 함수에서 입력 받는 값 : parameter
+ * 실제 입력하는 값 : argument
+ */
+
+function multiply(x, y) {   // 여러 개의 parameter 가능
+    console.log(x * y);
+}
+multiply(2, 4); // 8
+
+function multiply(x, y = 10) {   // default parameter 
+    console.log(x * y);
+}
+multiply(2);    // 20
+
+/**
+ * 반환 받기 
+ * return
+ */
+function multiply(x, y) {
+    return x * y;
+}
+const result = multiply(2, 4);  // 반환 값은 변수에 저장
+
+/**
+ * Arrow 함수
+ */
+const multiply2 = (x, y) => {
+    return x * y;
+}
+console.log(multiply2(3, 4));
+
+// 훨씬 간단한 arrow 함수
+const multiply3 = (x, y) => x * y;
+console.log(multiply3(4, 5));
+
+// 훨씬 더 간단한 arrow 함수 (parameter가 1개일 경우)
+const multiply4 = x => x * 2;
+console.log(multiply4(2));
+console.log('-----------');
+
+// chaining
+const multiply5 = x => y => z => `x: ${x} y: ${y} z: ${z}`;
+console.log(multiply5(2)(5)(7));
+// multiply5 함수를 일반 함수로 변경
+function multiply6(x) {
+    return function(y) {
+        return function(z) {
+            return `x: ${x} y: ${y} z: ${z}`;
+        }
+    }
+}
+console.log(multiply6(3)(4)(5));
+console.log('-----------');
+
+const multiplyTwo = function(x, y) {
+    return x * y;
+}
+console.log(multiplyTwo(4, 5));
+
+const multiplyThree = function(x, y, z) {
+    console.log(arguments); // 인자 출력
+    return x * y * z;
+}
+console.log(multiplyThree(3, 4, 5));
+
+// argument 개념을 이용한 함수
+const multiplyAll = function (...arguments) {
+    return Object.values(arguments).reduce((a, b) => a * b, 1);
+}
+console.log(multiplyAll(3, 4, 5, 6, 7, 8, 9, 10));
+
+// immediately invoked function : 함수 즉시 실행 (!)
+(function(x, y) {
+    console.log(x * y);
+})(4, 5);
+
+console.log(typeof multiply);
+console.log(multiply instanceof Object);    // 함수도 객체
+
+
+
+
+
+/**
+ * try...catch
+ * 
+ * 1) 발생 시킬 때 -> 'error를 던진다'라고 표현 (throw)
+ * 2) 명시적으로 인지할 때 -> 'error를 잡는다'라고 표현 (catch)
+ */
+
+function runner() {
+    try {
+        console.log('Hello');
+
+        // throw new Error('문제 발생!');   // 에러 발생
+
+        console.log('Jisoo !')  // 출력되지 않음
+    } catch (e) {   // 던진 error를 잡음
+        console.log('* catch *');
+        console.log(e);
+    } finally {     // error 발생 여부와 상관 없이 실행됨 (optional)
+        console.log('* finally *');
+    }
+    
+    
+}
+
+runner();
