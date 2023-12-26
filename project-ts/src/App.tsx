900e853cfb820c98986e18dd9d6268bc3475dd28
StopSoo <jpy1030@naver.com>
2023-12-26 15:45:20 +0900

    231226 ts component study

--- project-ts/src/App.tsx
+++ project-ts/src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* 1. Function 형태 */
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
+/* 2. Class 형태 */
+// 이런 식으로 props와 state를 모아서 작성하기도 한다.
+export interface AppProps {
+  name: string;
+}
+
+interface AppState {
+  age: number;
+}
+// generic 첫 번째는 props 자리 -> { 이름: 자료형; }
+// generic 두 번째는 state 자리 -> { 이름: 자료형; }
+class App extends React.Component<AppProps, AppState> {
+  // state 초기화 방법 1
+  // public state: { age: number; } = {
+  //   age: 24
+  // };
+  // state 초기화 방법 2
+  constructor(props: AppProps) {
+    super(props); // super() 반드시 작성
+    this.state = {
+      age: 24
+    };
+    setInterval(() => {
+      // 바뀌는 부분만 작성
+      this.setState({
+        age: this.state.age + 1
+      });
+    }, 2000);
+  }
+  render() {
+    return (
+      <div className="App">
+        { this.props.name }, { this.state.age }
+      </div>
+    );
+  }
 }
 
 export default App;
--- project-ts/src/index.tsx
+++ project-ts/src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,10 +7,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App name="Jisoo"/>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
