dbff1115341b8cb2d178a477de46c02e6fe7f3f4
StopSoo <jpy1030@naver.com>
2024-01-05 01:28:28 +0900

    <feat>: html 객체로 생성

--- React.js Study/lecture_react/2_react/js/main.js
+++ React.js Study/lecture_react/2_react/js/main.js
@@ -2,9 +2,20 @@
 // 여러 줄 사용 시 소괄호()로 묶어주기
 // class는 예약어이므로 className이라는 키워드를 사용
 const element = (
-  <header>
-    <h2 className="container">검색</h2>
-  </header>
+  <>
+    <header>
+      <h2 className="container">검색</h2>
+    </header>
+    <div className="container">
+      <form>
+        <input type="text" placeholder="검색어를 입력하세요" autoFocus />
+        <button type="reset" className="btn-reset"></button>
+      </form>
+    </div>
+  </>
 );
 
-ReactDOM.render(element, document.querySelector('#app'));
+ReactDOM.render(
+  element, 
+  document.querySelector('#app')
+);
