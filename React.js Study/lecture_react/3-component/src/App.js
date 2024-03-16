7609107fb753dffa1959daf100c96a704f4530c1
StopSoo <jpy1030@naver.com>
2024-03-16 17:48:46 +0900

    <refactor>: state 끌어올리기

--- React.js Study/lecture_react/3-component/src/App.js
+++ React.js Study/lecture_react/3-component/src/App.js
@@ -3,6 +3,22 @@ import Header from "./components/Header.js";
 import SearchForm from "./components/SearchForm.js";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchKeyword: "",
+    };
+  }
+  
+  handleChangeInput(searchKeyword) {
+    if (searchKeyword.length <= 0) {
+      this.handleReset();
+    }
+    
+    this.setState({ searchKeyword });
+  }
+
   search() {
 
   }
@@ -18,6 +34,8 @@ export default class App extends React.Component {
         <div className="container">
           {/* SearchForm - handleSubmit 함수를 통해 넘어온 searchKeyword */}
           <SearchForm 
+            value={ this.state.searchKeyword }
+            onChange={(value) => this.handleChangeInput(value)}
             onSubmit={(searchKeyword) => this.search(searchKeyword)}
             onReset={() => this.handleReset()}
           />
--- React.js Study/lecture_react/3-component/src/components/SearchForm.js
+++ React.js Study/lecture_react/3-component/src/components/SearchForm.js
@@ -1,53 +1,36 @@
 import React from "react";
-
 // export default : 해당 컴포넌트를 기본 모듈로 등록
 // class와 function 중 SearchForm 컴포넌트는 state를 사용해야 하므로 class를 선택
-export default class SearchForm extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      searchKeyword: "",
-    };
-  }
-
-  handleSubmit() {
+// state를 사용할 필요가 없어졌으므로 function으로 변경
+// function component로 들어오는 걸 props로 하거나, 해체 문법을 이용헤 다 나열하거나 !
+const SearchForm = ({ value, onChange, onSubmit, onReset }) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    this.props.onSubmit(this.state.searchKeyword);  // props의 callback 함수 호출
+    onSubmit();
   }
 
-  handleReset() {
-    this.props.onReset();
+  const handleReset = () => {
+    onReset();
   }
 
-  handleChangeInput(event) {
-    const searchKeyword = event.target.value;
-
-    // 검색어 삭제 시 검색 결과 삭제 함수 호출
-    if (searchKeyword.length <= 0) {
-      this.handleReset();
-    }
-
-    this.setState({ searchKeyword });
+  const handleChangeInput = (event) => {
+    onChange(event.target.value);
   }
 
-  render() {
-    return (
-      <form 
-        onSubmit={event => this.handleSubmit(event)} 
-        onReset={() => this.handleReset()}
-      >
-        <input 
-          type="text"
-          placeholder="검색어를 입력하세요"
-          autoFocus
-          value={this.state.searchKeyword}
-          onChange={(event) => this.handleChangeInput(event)}
-        />
-        {this.state.searchKeyword.length > 0 && (
-          <button type="reset" className="btn-reset" />
-        )}
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit} onReset={handleReset}>
+      <input 
+        type="text"
+        placeholder="검색어를 입력하세요"
+        autoFocus
+        value={value}
+        onChange={handleChangeInput}
+      />
+      {value.length > 0 && (
+        <button type="reset" className="btn-reset" />
+      )}
+    </form>
+  );
 }
+
+export default SearchForm;
