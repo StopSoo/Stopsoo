0fd61e1fd628b7eb629db3ad96e9bd33e2f0ca3a
StopSoo <jpy1030@naver.com>
2024-03-18 23:19:15 +0900

    <refactor>: 컴포넌트 조합

--- React.js Study/lecture_react/3-component/src/components/HistoryList.js
+++ React.js Study/lecture_react/3-component/src/components/HistoryList.js
@@ -2,32 +2,48 @@ import React from 'react';
 import List from "./List.js";
 import { formatRelativeDate } from '../helpers.js';
 
-export default class HistoryList extends List {
+export default class HistoryList extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      historyList: [],
+    }
+  }
+  
   componentDidMount() {
     this.fetch();
   }
 
   fetch() {
-    const data = store.getHistoryList();
-    this.setState({ data });
+    const historyList = store.getHistoryList();
+    this.setState({ historyList });
   }
 
-  handleClickRemoveHistory(event, keyword) {
+  handleClickRemove(event, keyword) {
     event.stopPropagation();  // 다른 쪽 핸들러의 동작을 차단
     store.removeHistory(keyword); // 키워드 삭제 요청
     this.fetch();
   }
 
-  renderItem(item) {
+  render() {
     return (
-      <>
-        <span>{item.keyword}</span>
-        <span className="date">{formatRelativeDate(item.date)}</span>
-        <button
-          className='btn-remove'
-          onClick={(event) => this.handleClickRemoveHistory(event, item.keyword)}
-        />
-      </>
+      <List 
+        data={this.state.historyList}
+        onClick={this.props.onClick}
+        renderItem={(item) => {
+          return (
+            <>
+              <span>{item.keyword}</span>
+              <span className="date">{formatRelativeDate(item.date)}</span>
+              <button
+                className='btn-remove'
+                onClick={(event) => this.handleClickRemove(event, item.keyword)}
+              />
+            </>
+          );
+        }}
+      />
     );
   }
 }
