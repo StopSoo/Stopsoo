35902519eed703d1418f3e977808aa501ed90fcb
StopSoo <jpy1030@naver.com>
2024-03-18 23:41:13 +0900

    <refactor>: 특수화 적용

--- React.js Study/lecture_react/3-component/src/components/HistoryList.js
+++ React.js Study/lecture_react/3-component/src/components/HistoryList.js
@@ -21,28 +21,20 @@ export default class HistoryList extends React.Component {
   }
 
   handleClickRemove(event, keyword) {
-    event.stopPropagation();  // 다른 쪽 핸들러의 동작을 차단
     store.removeHistory(keyword); // 키워드 삭제 요청
     this.fetch();
   }
 
   render() {
+    const { onClick } = this.props;
+    const { historyList } = this.state;
+
     return (
       <List 
-        data={this.state.historyList}
-        onClick={this.props.onClick}
-        renderItem={(item) => {
-          return (
-            <>
-              <span>{item.keyword}</span>
-              <span className="date">{formatRelativeDate(item.date)}</span>
-              <button
-                className='btn-remove'
-                onClick={(event) => this.handleClickRemove(event, item.keyword)}
-              />
-            </>
-          );
-        }}
+        data={historyList}
+        onClick={onClick}
+        hasDate
+        onRemove={(keyword) => this.handleClickRemove(keyword)}
       />
     );
   }
