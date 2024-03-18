b251caff63e8a9202fe7b0b936a1230182a0591f
StopSoo <jpy1030@naver.com>
2024-03-18 23:07:59 +0900

    <refactor>: 리액트 문서 적용 - 컴포넌트 담기

--- React.js Study/lecture_react/3-component/src/components/KeywordList.js
+++ React.js Study/lecture_react/3-component/src/components/KeywordList.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import List from "./List.js";
 
-export default class KeywordList extends List {
-  componentDidMount() {
-    const data = store.getKeywordList();
+export default class KeywordList extends React.Component {
+  constructor() {
+    super();
 
-    this.setState({
-      data,
-    });
+    this.state = {
+      keywordList: [],
+    }
+  }
+
+  componentDidMount() {
+    const keywordList = store.getKeywordList();
+    this.setState({ keywordList });
+  }
+
+  render() {
+    return (
+      <List 
+        data={this.state.keywordList} 
+        onClick={this.props.onClick} 
+        renderItem={(item, index) => {
+          return (
+            <>
+              <span className='number'>{index+1}</span>
+              <span>{item.keyword}</span>
+            </>
+          );
+        }}
+      />
+    );
   }
 
   renderItem(item, index) {
