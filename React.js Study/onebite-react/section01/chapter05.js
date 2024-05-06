59374b9162bc4726b73a54515fd65c87bfb295d7
StopSoo <jpy1030@naver.com>
2024-05-06 21:38:00 +0900

    240506 react study

---
+++ React.js Study/onebite-react/section01/chapter05.js
@@ -0,0 +1,31 @@
+// 1. Number Type
+// 웬만한 숫자 모두 가능
+// 연산 가능
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// 2. String Type
+// 쌍따옴표 or 작은 따옴표
+// 문자열끼리 덧셈 연산 가능
+let myName = "정지수";
+let myLocation = "양주";
+// Template Literal Grammar : 백틱(`)과 ${}를 이용해 문자열을 동적으로 활용 가능
+let introduceText = `${myName}는 ${myLocation}에 거주한다.`;
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type : 아무 것도 없다
+let empty = null;
+
+// 5. Undefined Type
+let none;
+console.log(none);  // undefined
+
