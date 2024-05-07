c81bc5fcb2688e3cfa662fe53806d041890421f5
StopSoo <jpy1030@naver.com>
2024-05-07 17:58:15 +0900

    240507 react study : nodeJS

--- React.js Study/onebite-react/section03/src/index.js
+++ React.js Study/onebite-react/section03/src/index.js
@@ -14,3 +14,11 @@
 import { add, sub } from "./math.js";
 console.log(add(1, 2));
 console.log(sub(5, 3));
+
+// npm i randomcolor
+// package-lock.json : package.json 파일보다 더 구체적
+// node_modules 파일 안에 다운 받은 라이브러리 파일이 생성됨
+// npm에서 다운로드 받은 라이브러리 사용 예시
+import randomColor from "randomcolor";  // 라이브러리 import
+const color = randomColor();
+console.log(color); // 랜덤 색깔 코드가 출력됨
